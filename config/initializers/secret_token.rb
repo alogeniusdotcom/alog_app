dc08e35cf4736c518f90791030d663f06a764bc7
Alan Turner <a.turner@jaygenius.com>
2013-11-18 00:30:15 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlogApp::Application.config.secret_token = '25302f496aac256d77abd49a0be67dbba1d5ec54e4ac4e4d404091084f69cf96d867b529313144fd923c3f0b16c902efed753fde9b9a3ba05f21b019bfaa61df'
